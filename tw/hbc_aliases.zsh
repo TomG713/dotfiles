c35191cfeb909379a84b43fa8429965c047a1ba5
gambtho <tgamble@thoughtworks.com>
2016-10-12 13:42:07 -0400

    updates from hbc setup

---
+++ tw/hbc_aliases.zsh
@@ -0,0 +1,38 @@
+#export JAVA_HOME="$(/usr/libexec/java_home -v 1.8)"
+
+#ALIASES FOR DOCKER
+alias dm="docker-machine"
+alias docker-machien="docker-machine"
+alias dc="docker"
+alias dc="docker-compose"
+alias resetdocker="unset DOCKER_TLS_VERIFY && unset DOCKER_CERT_PATH && unset DOCKER_MACHINE_NAME && unset DOCKER_HOST"
+
+# ALIASES FOR HBC 
+alias saks-website="cd /workspace/hbc/saks_website"
+alias saks-backend="cd /workspace/hbc/saks_website_backend"
+
+alias pds="cd /workspace/hbc/product-detail-service"
+alias pas="cd /workspace/hbc/product-array-service"
+alias hbc="cd /workspace/hbc"
+
+alias qaslot8bm="ssh bmadmin@qaslot8.saksdirect.com"
+alias qaslot8docker="ssh vndhshin@hd5qdkr08lx.digital.hbc.com"
+alias qaslot8endeca="ssh endadmin@qaslot8.saksdirect.com"
+
+alias devslot8docker="ssh vndhshin@hd5ddkr08lx.digital.hbc.com"
+alias devslot8bm="ssh bmadmin@devslot8.saksdirect.com"
+alias devslot8endeca="ssh endadmin@devslot8.saksdirect.com"
+
+alias qa33ms="ssh vndhshin@hd5qdkr33lx.digital.hbc.com"
+alias qa33endeca="ssh endadmin@qaslot33.digital.hbc.com"
+alias dev33ms="ssh vndhshin@hd5ddkr33lx.digital.hbc.com"
+alias dev33endeca="ssh endadmin@devslot33.digital.hbc.com"
+alias qa33ms="ssh vndhshin@hd5qdkr33lx.digital.hbc.com"
+alias qa33endeca="ssh endadmin@qaslot33.digital.hbc.com"
+alias dev33ms="ssh vndhshin@hd5ddkr33lx.digital.hbc.com"
+alias dev33endeca="ssh endadmin@devslot33.digital.hbc.com"
+
+alias qa34ms="ssh vndhshin@hd5qdkr34lx.digital.hbc.com"
+alias qa34endeca="ssh endadmin@qaslot34.digital.hbc.com"
+alias dev34ms="ssh vndhshin@hd5ddkr34lx.digital.hbc.com"
+alias dev34endeca="ssh endadmin@devslot34.digital.hbc.com"
