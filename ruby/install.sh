caf08facfbdcd56507afc130867ae731c122a3ea
gambtho <thomasgamble2@gmail.com>
2019-08-05 15:14:54 -0700

    ruby updates

--- ruby/install.sh
+++ ruby/install.sh
@@ -1,9 +1,11 @@
-RUBY_VERSION=2.4.1
+RUBY_VERSION=2.6.3
 
 if test ! $(which rbenv)
 then
   echo
   echo "Installing Ruby tools and Ruby ${RUBY_VERSION}"
+  git clone https://github.com/rbenv/rbenv.git ~/.rbenv
+  git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
   eval "$(rbenv init -)"
   rbenv install ${RUBY_VERSION} --skip-existing
   rbenv global ${RUBY_VERSION}
