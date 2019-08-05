e86a007f696838f36b3340df67945e91d3650be9
gambtho <thomasgamble2@gmail.com>
2019-08-05 15:34:50 -0700

    code insiders alias

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,3 +1,4 @@
 alias reload!='. ~/.zshrc'
 alias vi='nvim'
+alias code="/mnt/c/Users/thgamble/AppData/Local/Programs/Microsoft\ VS\ Code\ Insiders/Code\ -\ Insiders.exe"
 alias ls='ls --color=auto'
