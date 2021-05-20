f858c467a5180b07c598230b16a470582a23f706
gambtho <Thomas.Gamble@microsoft.com>
2021-05-19 21:00:30 -0400

    minor updates

--- go/path.zsh
+++ go/path.zsh
@@ -1,3 +1,3 @@
 source $HOME/.gvm/scripts/gvm
 export GOPATH=~/workspace/go
-export PATH=$PATH:/usr/local/go/bin:$GOPATH/bin
+export PATH=$PATH:/usr/local/go/bin:$GOPATH/bin:$GOPATH/src/goms.io/aks/rp/bin
