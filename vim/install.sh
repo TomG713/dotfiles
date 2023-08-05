70c1a1de95bf938dfac11f9513d3d58a14c04e3d
Thomas.Gamble <Thomas.Gamble@microsoft.com>
2023-08-05 02:21:21 +0000

    linux updates

--- vim/install.sh
+++ vim/install.sh
@@ -8,7 +8,7 @@ then
 	pip install setuptools
 	pip3 install pynvim
 	pip2 install pynvim
-    git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+        git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 	vim +PluginInstall +qall
 fi
 echo "> completed"
