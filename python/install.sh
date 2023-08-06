e4127e7f58a1ce055d20b0094fc79cb1ddfef6cf
gambtho <Thomas.Gamble@microsoft.com>
2023-08-06 11:35:25 -0400

    pyenv update

--- python/install.sh
+++ python/install.sh
@@ -4,6 +4,8 @@ if test ! $(which pyenv)
 then
     echo "Installing pyenv for you."
     curl https://pyenv.run | bash
+    export PYENV_ROOT="$HOME/.pyenv"
+    export PATH="$PYENV_ROOT/bin:$PATH"
     eval "$(pyenv init -)"
 fi
 
