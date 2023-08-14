c9f91185e2568b21912d20b52855aa23bcdd5e3b
gambtho <Thomas.Gamble@microsoft.com>
2023-08-14 11:48:06 -0400

    minor mac updates

--- aks/aliases.zsh
+++ aks/aliases.zsh
@@ -4,5 +4,6 @@ alias gowork='aksgo && cd ${GOPATH}'
 alias gorp='gowork && cd src/goms.io/aks/rp'
 alias idweb='kinit THGAMBLE@NORTHAMERICA.CORP.MICROSOFT.COM && open -a safari https://idweb/'
 alias aksdev='~/go/src/goms.io/aks/rp/bin/aksdev'
-alias startdev='az vm start --name tg-202308 --resource-group thgamble-devbox'
+alias startdev='az vm start --name tg-202308 --resource-group thgamble-devbox && tailscale down && tailscale up && ssh devbox'
+alias devboxsub='az login && az account set -s c1089427-83d3-4286-9f35-5af546a6eb67'
 
--- aks/install.sh
+++ aks/install.sh
@@ -1,8 +1,22 @@
 #!/usr/bin/env bash
 
+# automate this later
+
 if test ! $(which tailscale)
 then
     echo "Install tailscale!"
     echo "go install tailscale.com/cmd/tailscale{,d}@v1.46.1"
     echo "https://github.com/tailscale/tailscale/wiki/Tailscaled-on-macOS"
+fi
+
+if test ! $(which ktx)
+then 
+	echo "Install ktx"
+    	echo "curl https://raw.githubusercontent.com/blendle/kns/master/bin/ktx -o /usr/local/bin/ktx && chmod +x $_"
+fi
+
+if test ! $(which kns)
+then
+	echo "Install kns"
+	echo "curl https://raw.githubusercontent.com/blendle/kns/master/bin/kns -o /usr/local/bin/kns && chmod +x $_"
 fi
