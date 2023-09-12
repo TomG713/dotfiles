a75eb9c29e37e22d633fb60653b3c54f62fdb24b
gambtho <Thomas.Gamble@microsoft.com>
2023-09-11 22:57:39 -0400

    cleanup

--- aks/install.sh
+++ aks/install.sh
@@ -1,16 +1,14 @@
 #!/usr/bin/env bash
 
-# automate this later
-
 
 if test ! $(which ktx)
 then 
-	echo "Install ktx"
-    	echo "curl https://raw.githubusercontent.com/blendle/kns/master/bin/ktx -o /usr/local/bin/ktx && chmod +x $_"
+	echo "Installing ktx"
+    	sudo curl https://raw.githubusercontent.com/blendle/kns/master/bin/ktx -o /usr/local/bin/ktx && sudo chmod +x $_
 fi
 
 if test ! $(which kns)
 then
-	echo "Install kns"
-	echo "curl https://raw.githubusercontent.com/blendle/kns/master/bin/kns -o /usr/local/bin/kns && chmod +x $_"
+	echo "Installing kns"
+	sudo curl https://raw.githubusercontent.com/blendle/kns/master/bin/kns -o /usr/local/bin/kns && sudo chmod +x $_
 fi
