a29220a32baa5d87c4f3b379a7c45d4e79a2a5a5
gambtho <Thomas.Gamble@microsoft.com>
2023-09-19 21:00:45 -0400

    cleanup and start move to antigen

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -1,3 +1,1 @@
-alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
 alias gclean='git remote prune origin && git reflog expire --expire=now --all && git gc --prune=now --aggressive && git branch | grep -v "master" | xargs git branch -D'
-alias gs='git status -sbuno' # upgrade your git if -sb breaks for you. it's fun.
