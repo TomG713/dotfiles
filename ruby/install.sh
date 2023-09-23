aa8c0b7e629c0f7639c7c2c294931b3fbe6b9d51
Thomas.Gamble <Thomas.Gamble@microsoft.com>
2023-09-23 19:28:10 +0000

    fix ruby install

--- ruby/install.sh
+++ ruby/install.sh
@@ -1,6 +1,15 @@
+#!/usr/bin/env zsh
+
 RUBY_VERSION=3.2.2
 
-
+function install_ruby() {
+  if [[ "$OSTYPE" == "linux*" ]]; then 
+    echo "using openssl dir /usr/lib/ssl, if this fails, try openssl version -d"
+    RUBY_CONFIGURE_OPTS=--with-openssl-dir=/usr/lib/ssl && rbenv install "${RUBY_VERSION}" --skip-existing
+  else
+    rbenv install ${RUBY_VERSION} --skip-existing
+  fi
+}
 
 if test ! $(which rbenv)
 then
@@ -9,16 +18,7 @@ then
   git clone https://github.com/rbenv/rbenv.git ~/.rbenv
   git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
   eval "$(~/.rbenv/bin/rbenv init - zsh)"
-<<<<<<< HEAD
-  if [[ "$OSTYPE" == "linux*" ]]; then 
-    echo "using openssl dir /usr/lib/ssl, if this fails, try openssl version -d"
-    RUBY_CONFIGURE_OPTS=--with-openssl-dir=/usr/lib/ssl && rbenv install "${RUBY_VERSION}" --skip-existing
-  else
-    rbenv install ${RUBY_VERSION} --skip-existing
-  fi
-=======
   install_ruby
->>>>>>> a482e4a4dbe671d8d54f9731bbf8fb395b9c987a
   rbenv global ${RUBY_VERSION}
   gem install bundler
   rbenv rehash
@@ -35,11 +35,3 @@ then
 fi
 
 
-function install_ruby() {
-  if [[ "$OSTYPE" == "linux*" ]]; then 
-    echo "using openssl dir /usr/lib/ssl, if this fails, try openssl version -d"
-    RUBY_CONFIGURE_OPTS=--with-openssl-dir=/usr/lib/ssl && rbenv install "${RUBY_VERSION}" --skip-existing
-  else
-    rbenv install ${RUBY_VERSION} --skip-existing
-  fi
-}
