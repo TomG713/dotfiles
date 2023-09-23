7093100707792c5ca63be42e2cf4cd26b4b3ea6e
gambtho <Thomas.Gamble@microsoft.com>
2023-09-23 15:18:45 -0400

    cleanup ruby

--- ruby/install.sh
+++ ruby/install.sh
@@ -1,5 +1,7 @@
 RUBY_VERSION=3.2.2
 
+
+
 if test ! $(which rbenv)
 then
   echo
@@ -7,7 +9,12 @@ then
   git clone https://github.com/rbenv/rbenv.git ~/.rbenv
   git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
   eval "$(~/.rbenv/bin/rbenv init - zsh)"
-  rbenv install ${RUBY_VERSION} --skip-existing
+  if [[ "$OSTYPE" == "linux*" ]]; then 
+    echo "using openssl dir /usr/lib/ssl, if this fails, try openssl version -d"
+    RUBY_CONFIGURE_OPTS=--with-openssl-dir=/usr/lib/ssl && rbenv install "${RUBY_VERSION}" --skip-existing
+  else
+    rbenv install ${RUBY_VERSION} --skip-existing
+  fi
   rbenv global ${RUBY_VERSION}
   gem install bundler
   rbenv rehash
