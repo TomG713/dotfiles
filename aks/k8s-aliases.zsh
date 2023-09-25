ac05bb7ec6e5bb50694e0ec5c644c89b1920f616
gambtho <Thomas.Gamble@microsoft.com>
2023-09-25 17:44:36 -0400

    update k8s aliases

--- aks/k8s-aliases.zsh
+++ aks/k8s-aliases.zsh
@@ -133,19 +133,6 @@ fi
 alias kbroken='kubectl get pods --all-namespaces -o wide | grep -v Running'
 alias kdelete='kubectl delete pods --field-selector status.phase!=Running --all-namespaces'
 
-if (( ! $+commands[kubectl] )); then
-  return
-fi
-
-# If the completion file doesn't exist yet, we need to autoload it and
-# bind it to `kubectl`. Otherwise, compinit will have already done that.
-if [[ ! -f "$ZSH_CACHE_DIR/completions/_kubectl" ]]; then
-  typeset -g -A _comps
-  autoload -Uz _kubectl
-  _comps[kubectl]=_kubectl
-fi
-
-kubectl completion zsh 2> /dev/null >| "$ZSH_CACHE_DIR/completions/_kubectl" &|
 
 # Notes
 # 
