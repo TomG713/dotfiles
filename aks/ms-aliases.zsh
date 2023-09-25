eb2c39bc6ec56e88e0f601543477459bee6a301c
gambtho <Thomas.Gamble@microsoft.com>
2023-09-25 18:17:21 -0400

    update devbox name

--- aks/ms-aliases.zsh
+++ aks/ms-aliases.zsh
@@ -9,7 +9,7 @@ alias aksdev='~/go/src/goms.io/aks/rp/bin/aksdev'
 
 # devbox
 alias starthyper='open -a "Azure VPN Client" && ssh hyper'
-alias startdev='devboxsub && open -a "Azure VPN Client" && az vm start --name tg-202308 --resource-group thgamble-devbox && ssh devbox'
+alias startdev='devboxsub && open -a "Azure VPN Client" && az vm start --name tg --resource-group thgamble-devbox && ssh devbox'
 alias devboxsub='az login && az account set -s c1089427-83d3-4286-9f35-5af546a6eb67'
 alias devsub='az account set -s d0ecd0d2-779b-4fd0-8f04-d46d07f05703'
 
