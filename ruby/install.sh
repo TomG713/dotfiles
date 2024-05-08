5fad11db330f602b8ff3721797ae60360a625895
gambtho <Thomas.Gamble@microsoft.com>
2024-05-08 00:49:33 -0400

    more ruby fun

--- ruby/install.sh
+++ ruby/install.sh
@@ -1,24 +1,25 @@
 #!/usr/bin/env zsh
 
-RUBY_VERSION=3.2.2
+RUBY_VERSION=3.3.1
 
 function install_ruby() {
-  if [[ "$OSTYPE" == "linux*" ]]; then 
-    echo "using openssl dir /usr/lib/ssl, if this fails, try openssl version -d"
-    RUBY_CONFIGURE_OPTS=--with-openssl-dir=/usr/lib/ssl && rbenv install "${RUBY_VERSION}" --skip-existing
-  else
-    rbenv install ${RUBY_VERSION} --skip-existing
-  fi
+  rbenv install ${RUBY_VERSION} --skip-existing
+  rbenv global ${RUBY_VERSION}
 }
 
 if ! (( $+commands[rbenv] ))
 then
   echo "Installing Ruby tools and Ruby ${RUBY_VERSION}"
-  git clone https://github.com/rbenv/rbenv.git ~/.rbenv
-  git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
+  if [[ "$OSTYPE" == "linux*" ]]; then 
+    # echo "using openssl dir /usr/lib/ssl, if this fails, try openssl version -d"
+    # RUBY_CONFIGURE_OPTS=--with-openssl-dir=/usr/lib/ssl && rbenv install "${RUBY_VERSION}" --skip-existing
+    sudo apt install rbenv
+  else
+    git clone https://github.com/rbenv/rbenv.git ~/.rbenv
+    git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
+  fi
   eval "$(~/.rbenv/bin/rbenv init - zsh)"
   install_ruby
-  rbenv global ${RUBY_VERSION}
   gem install bundler
   rbenv rehash
 fi
@@ -28,7 +29,6 @@ then
   # Update
   eval "$(command rbenv init -)"
   install_ruby
-  rbenv global $RUBY_VERSION
   echo "> updating gems"
   gem update && gem update --system 2>&1
 fi
