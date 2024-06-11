84bacccb1a79b910da4724b4e35efaba5375ed8a
gambtho <Thomas.Gamble@microsoft.com>
2024-06-11 19:48:40 -0400

    bump versions

--- go/install.sh
+++ go/install.sh
@@ -4,6 +4,6 @@ set -e
 
 source "$(dirname "$0")/../bin/common.sh"
 
-export GO_VERSION=1.22.3
+export GO_VERSION=1.22.4
 
 install_or_update "go" "$GO_VERSION" "go version | awk '{print \$3}' | sed 's/go//'"
--- python/install.sh
+++ python/install.sh
@@ -4,6 +4,6 @@ set -e
 
 source "$(dirname "$0")/../bin/common.sh"
 
-export PYTHON_VERSION=3.10.2
+export PYTHON_VERSION=3.12.4
 
 install_or_update "python" "$PYTHON_VERSION" "python --version 2>&1 | awk '{print \$2}'"
