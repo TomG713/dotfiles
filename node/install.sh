78e832b1640bcb8761ad39eeb555e8482343b637
gambtho <Thomas.Gamble@microsoft.com>
2024-06-19 16:45:22 -0400

    version bump

--- node/install.sh
+++ node/install.sh
@@ -4,6 +4,6 @@ set -e
 
 source "$(dirname "$0")/../bin/common.sh"
 
-export NODE_VERSION=22.2.0  # Set your desired Node.js version here
+export NODE_VERSION=22.3.0  # Set your desired Node.js version here
 
 install_or_update "node" "$NODE_VERSION" "node -v | sed 's/v//'"
--- ruby/install.sh
+++ ruby/install.sh
@@ -4,7 +4,7 @@ set -e
 
 source "$(dirname "$0")/../bin/common.sh"
 
-export RUBY_VERSION=3.3.2
+export RUBY_VERSION=3.3.3
 
 install_or_update "ruby" "$RUBY_VERSION" "ruby --version 2>&1 | awk '{print \$2}'"
 
