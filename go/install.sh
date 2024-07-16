0165d9990e36495cd571e2f5b2f07be9f38a134d
Thomas Gamble <Thomas.Gamble@microsoft.com>
2024-07-16 10:50:50 -0400

    version bump

--- go/install.sh
+++ go/install.sh
@@ -4,6 +4,6 @@ set -e
 
 source "$(dirname "$0")/../bin/common.sh"
 
-export GO_VERSION=1.22.4
+export GO_VERSION=1.22.5
 
 install_or_update "go" "$GO_VERSION" "go version | awk '{print \$3}' | sed 's/go//'"
