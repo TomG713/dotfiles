d815987a2cd90faad9538afaa5b6f4922002fb81
Thomas Gamble <thomasgamble2@gmail.com>
2024-07-20 17:27:05 -0400

    wsl specific apt

--- java/install.sh
+++ java/install.sh
@@ -4,6 +4,6 @@ set -e
 
 source "$(dirname "$0")/../bin/common.sh"
 
-export JAVA_VERSION=22.0.1
+export JAVA_VERSION=22.0.2
 
 install_or_update "java" "$JAVA_VERSION" "java -version 2>&1 | awk -F '\"' '/version/ {print \$2}'"
--- node/install.sh
+++ node/install.sh
@@ -4,6 +4,6 @@ set -e
 
 source "$(dirname "$0")/../bin/common.sh"
 
-export NODE_VERSION=22.4.1  # Set your desired Node.js version here
+export NODE_VERSION=22.5.1  # Set your desired Node.js version here
 
 install_or_update "node" "$NODE_VERSION" "node -v | sed 's/v//'"
