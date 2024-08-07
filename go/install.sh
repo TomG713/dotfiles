e6299832bcb1365f2d39ae4b912a0f8143a38e69
Thomas Gamble <thomasgamble2@gmail.com>
2024-08-07 12:14:39 -0400

    bump golang/node

--- go/install.sh
+++ go/install.sh
@@ -4,6 +4,6 @@ set -e
 
 source "$(dirname "$0")/../bin/common.sh"
 
-export GO_VERSION=1.22.5
+export GO_VERSION=1.22.6
 
 install_or_update "go" "$GO_VERSION" "go version | awk '{print \$3}' | sed 's/go//'"
