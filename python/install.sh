7d277cc840b0966eace30b953456fd289646a9f1
Thomas Gamble <thomasgamble2@gmail.com>
2024-08-17 03:21:24 -0400

    bump versions

--- python/install.sh
+++ python/install.sh
@@ -4,6 +4,6 @@ set -e
 
 source "$(dirname "$0")/../bin/common.sh"
 
-export PYTHON_VERSION=3.12.4
+export PYTHON_VERSION=3.12.5
 
 install_or_update "python" "$PYTHON_VERSION" "python --version 2>&1 | awk '{print \$2}'"
