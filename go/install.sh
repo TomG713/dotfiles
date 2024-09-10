5541b7bbb2e047b3520fdef24d4685712e964f59
Thomas Gamble <thomasgamble2@gmail.com>
2024-09-10 00:34:00 -0400

    more bumps

--- go/install.sh
+++ go/install.sh
@@ -4,6 +4,6 @@ set -e
 
 source "$(dirname "$0")/../bin/common.sh"
 
-export GO_VERSION=1.23.0
+export GO_VERSION=1.23.1
 
 install_or_update "go" "$GO_VERSION" "go version | awk '{print \$3}' | sed 's/go//'"
