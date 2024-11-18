301dd0e1911e53864ccd942166c9c6c3ca61aaf8
Thomas Gamble <thomasgamble2@gmail.com>
2024-11-18 16:06:02 -0500

    more windows

---
+++ win/boxstarter.ps1
@@ -0,0 +1,135 @@
+# Description: Boxstarter Script
+# Author: Microsoft
+# Common dev settings for desktop app development
+
+# https://boxstarter.org/weblauncher
+# START https://boxstarter.org/package/nr/url?https://gist.github.com/mwrock/7382880/raw/f6525387b4b524b8eccef6ed4d5ec219c82c0ac7/gistfile1.txt
+
+Disable-UAC
+
+# Get the base URI path from the ScriptToCall value
+$bstrappackage = "-bootstrapPackage"
+$helperUri = $Boxstarter['ScriptToCall']
+$strpos = $helperUri.IndexOf($bstrappackage)
+$helperUri = $helperUri.Substring($strpos + $bstrappackage.Length)
+$helperUri = $helperUri.TrimStart("'", " ")
+$helperUri = $helperUri.TrimEnd("'", " ")
+$helperUri = $helperUri.Substring(0, $helperUri.LastIndexOf("/"))
+$helperUri += "/scripts"
+write-host "helper script base URI is $helperUri"
+
+function executeScript {
+    Param ([string]$script)
+    write-host "executing $helperUri/$script ..."
+	iex ((new-object net.webclient).DownloadString("$helperUri/$script"))
+}
+
+#--- Configuring Windows properties ---
+#--- Windows Features ---
+# Show hidden files, Show protected OS files, Show file extensions
+Set-WindowsExplorerOptions -EnableShowHiddenFilesFoldersDrives -EnableShowProtectedOSFiles -EnableShowFileExtensions
+
+#--- File Explorer Settings ---
+# will expand explorer to the actual folder you're in
+Set-ItemProperty -Path HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced -Name NavPaneExpandToCurrentFolder -Value 1
+#adds things back in your left pane like recycle bin
+Set-ItemProperty -Path HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced -Name NavPaneShowAllFolders -Value 1
+#opens PC to This PC, not quick access
+Set-ItemProperty -Path HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced -Name LaunchTo -Value 1
+#taskbar where window is open for multi-monitor
+Set-ItemProperty -Path HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced -Name MMTaskbarMode -Value 2
+
+
+#--- Uninstall unnecessary applications that come with Windows out of the box ---
+Write-Host "Uninstall some applications that come with Windows out of the box" -ForegroundColor "Yellow"
+
+#Referenced to build script
+# https://docs.microsoft.com/en-us/windows/application-management/remove-provisioned-apps-during-update
+# https://github.com/jayharris/dotfiles-windows/blob/master/windows.ps1#L157
+# https://gist.github.com/jessfraz/7c319b046daa101a4aaef937a20ff41f
+# https://gist.github.com/alirobe/7f3b34ad89a159e6daa1
+# https://github.com/W4RH4WK/Debloat-Windows-10/blob/master/scripts/remove-default-apps.ps1
+
+function removeApp {
+	Param ([string]$appName)
+	Write-Output "Trying to remove $appName"
+	Get-AppxPackage $appName -AllUsers | Remove-AppxPackage
+	Get-AppXProvisionedPackage -Online | Where DisplayName -like $appName | Remove-AppxProvisionedPackage -Online
+}
+
+$applicationList = @(
+	"Microsoft.BingFinance"
+	"Microsoft.3DBuilder"
+	"Microsoft.BingNews"
+	"Microsoft.BingSports"
+	"Microsoft.BingWeather"
+	"Microsoft.CommsPhone"
+	"Microsoft.Getstarted"
+	"Microsoft.WindowsMaps"
+	"*MarchofEmpires*"
+	"Microsoft.GetHelp"
+	"Microsoft.Messaging"
+	"*Minecraft*"
+	"Microsoft.MicrosoftOfficeHub"
+	"Microsoft.OneConnect"
+	"Microsoft.WindowsPhone"
+	"Microsoft.WindowsSoundRecorder"
+	"*Solitaire*"
+	"Microsoft.MicrosoftStickyNotes"
+	"Microsoft.Office.Sway"
+	"Microsoft.XboxApp"
+	"Microsoft.XboxIdentityProvider"
+	"Microsoft.XboxGameOverlay"
+	"Microsoft.XboxGamingOverlay"
+	"Microsoft.ZuneMusic"
+	"Microsoft.ZuneVideo"
+	"Microsoft.NetworkSpeedTest"
+	"Microsoft.FreshPaint"
+	"Microsoft.Print3D"
+	"Microsoft.People*"
+	"Microsoft.Microsoft3DViewer"
+	"Microsoft.MixedReality.Portal*"
+	"*Skype*"
+	"*Autodesk*"
+	"*BubbleWitch*"
+    	"king.com*"
+    	"G5*"
+	"*Dell*"
+	"*Facebook*"
+	"*Keeper*"
+	"*Netflix*"
+	"*Twitter*"
+	"*Plex*"
+	"*.Duolingo-LearnLanguagesforFree"
+	"*.EclipseManager"
+	"ActiproSoftwareLLC.562882FEEB491" # Code Writer
+	"*.AdobePhotoshopExpress"
+);
+
+foreach ($app in $applicationList) {
+    removeApp $app
+}
+
+choco install -y vscode
+choco install -y git --package-parameters="'/GitAndUnixToolsOnPath /WindowsTerminal'"
+choco install -y python
+choco install -y sysinternals
+
+choco install -y powershell-core
+choco install -y azure-cli
+Install-Module -Force Az
+
+
+#--- Browsers ---
+choco install -y googlechrome
+
+
+Update-SessionEnvironment #refreshing env due to Git install
+
+executeScript "WindowsTemplateStudio.ps1";
+executeScript "GetUwpSamplesOffGithub.ps1";
+
+#--- reenabling critial items ---
+Enable-UAC
+Enable-MicrosoftUpdate
+Install-WindowsUpdate -acceptEula
--- win/winget.ps1
+++ win/winget.ps1
@@ -23,7 +23,7 @@ Foreach ($app in $apps) {
     $listApp = winget list --exact -q $app.name
     if (![String]::Join("", $listApp).Contains($app.name)) {
         Write-host "Installing: " $app.name
-        winget install -e -h --accept-source-agreements --accept-package-agreements --id $app.name 
+        winget install -e --accept-source-agreements --accept-package-agreements --id $app.name 
     }
     else {
         Write-host "Skipping: " $app.name " (already installed)"
